2e36a7ecb6cc3a07ebcf97a7dda17083e50f77e7
= <->
2017-06-12 10:57:06 +0700

    update fourier extrapolation test

--- 04-real-work-started/fourier_extrapolation/main.py
+++ 04-real-work-started/fourier_extrapolation/main.py
@@ -11,8 +11,8 @@ line_price = graph.trace_line(close)
 wave_data_h10 = fourier_extrapolation(close, n_harm=10)
 wave_data_h20 = fourier_extrapolation(close, n_harm=20)
 
-line_wave_h10 = graph.trace_line(wave_data_h10)
-line_wave_h20 = graph.trace_line(wave_data_h20)
+line_wave_h10 = graph.trace_line(wave_data_h10, title='harmonic 10', color='red')
+line_wave_h20 = graph.trace_line(wave_data_h20, title='harmonic 20', color='blue')
 
 layout = graph.layout(slider=True)
 figure = graph.figure(layout, line_price, line_wave_h10, line_wave_h20)
