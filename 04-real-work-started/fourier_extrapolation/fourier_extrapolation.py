5c0bef5324c09a53ead5c311157b2c22f3f80c4a
= <->
2017-06-12 10:54:20 +0700

    test fourier_extrapolation

---
+++ 04-real-work-started/fourier_extrapolation/fourier_extrapolation.py
@@ -0,0 +1,23 @@
+from numpy import fft
+import numpy as np
+
+def fourier_extrapolation(x, n_harm = 10, n_predict=0):
+    #n_harm -> number of harmonics in model
+    x =  np.array(x)
+    n = x.size
+    t = np.arange(0, n)
+    p = np.polyfit(t, x, 1)         # find linear trend in x
+    x_notrend = x - p[0] * t        # detrended x
+    x_freqdom = fft.fft(x_notrend)  # detrended x in frequency domain
+    f = fft.fftfreq(n)              # frequencies
+    indexes = list(range(n))
+    # sort indexes by frequency, lower -> higher
+    indexes.sort(key = lambda i: np.absolute(f[i]))
+ 
+    t = np.arange(0, n + n_predict)
+    restored_sig = np.zeros(t.size)
+    for i in indexes[:1 + n_harm * 2]:
+        ampli = np.absolute(x_freqdom[i]) / n   # amplitude
+        phase = np.angle(x_freqdom[i])          # phase
+        restored_sig += ampli * np.cos(2 * np.pi * f[i] * t + phase)
+    return list(restored_sig + p[0] * t)
---
+++ 04-real-work-started/fourier_extrapolation/main.py
@@ -0,0 +1,21 @@
+from datautil.loader import data_from_api
+from plotlyutil import graph
+from plotly.plotly import plot
+from fourier_extrapolation import fourier_extrapolation
+
+data_frame = data_from_api('SET', '2HOUR')
+close = data_frame.Close
+
+line_price = graph.trace_line(close)
+
+wave_data_h10 = fourier_extrapolation(close, n_harm=10)
+wave_data_h20 = fourier_extrapolation(close, n_harm=20)
+
+line_wave_h10 = graph.trace_line(wave_data_h10)
+line_wave_h20 = graph.trace_line(wave_data_h20)
+
+layout = graph.layout(slider=True)
+figure = graph.figure(layout, line_price, line_wave_h10, line_wave_h20)
+plot(figure)
+
+
