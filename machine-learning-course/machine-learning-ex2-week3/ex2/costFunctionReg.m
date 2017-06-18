935ea028007a553356c7f8e68e6ebb2fa9043e4d
Tawan Thampipattanakul <tawan.tpptnk@gmail.com>
2017-06-18 18:16:59 +0700

    complete machine learning course exercise2 - week 3 part 2

--- machine-learning-course/machine-learning-ex2-week3/ex2/costFunctionReg.m
+++ machine-learning-course/machine-learning-ex2-week3/ex2/costFunctionReg.m
@@ -17,10 +17,22 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+constant = 1/m;
+constant_regularization = lambda/(2*m);
+constant_reg_grad = lambda/m;
 
+linear_prediction = X*theta;
+prediction = sigmoid(linear_prediction);
 
+%find cost
+cost_positive = -1 * (y'*(log(prediction)));
+cost_negative = -1 * ((1 .- y)'*(1 .- log(prediction)));
+cost_regularization = constant_regularization*sum(theta(2:end,1) .^ 2);
+J = constant*(cost_positive + cost_negative) + cost_regularization;
 
-
+%find gradient
+grad = constant*( X'*(prediction .- y)) .+ constant_reg_grad*theta;
+grad(1) -= constant_reg_grad*theta(1);
 
 % =============================================================
 
